40ccc94d5bfcc8c70b551d591b8fab180f4356aa
Martin Riggs <63644849+wanderrer@users.noreply.github.com>
2021-04-21 16:01:47 +0300

    Optimizing resource
    I have rewritten the code a bit, to be more efficient/faster and consume less resources.
    // Martin Riggs in the discord server

--- client/main.lua
+++ client/main.lua
@@ -3,14 +3,14 @@ QBCore = nil
 Citizen.CreateThread(function()
 	while QBCore == nil do
 		TriggerEvent('QBCore:GetObject', function(obj) QBCore = obj end)
-		Citizen.Wait(200)
+		Citizen.Wait(0)
 	end
 end)
 
 local closestDoorKey, closestDoorValue = nil, nil
 
 function DrawText3Ds(x, y, z, text)
-	SetTextScale(0.35, 0.35)
+    SetTextScale(0.35, 0.35)
     SetTextFont(4)
     SetTextProportional(1)
     SetTextColour(255, 255, 255, 215)
@@ -45,11 +45,126 @@ Citizen.CreateThread(function()
 end)
 
 local maxDistance = 1.25
+local drowtargetdoors = {}
+local isAuth = false
 
 Citizen.CreateThread(function()
 	while true do
-		Citizen.Wait(5)
-		local playerCoords, awayFromDoors = GetEntityCoords(PlayerPedId()), true
+		Citizen.Wait(0)
+		for i,j in ipairs(drowtargetdoors) do
+
+			if IsControlJustReleased(0, 38) then
+				if j.isAuth then
+					setDoorLocking(j.doorID, j.id)
+						j.status = "Locking.."
+						
+					if j.doorID.locked then
+					j.status = "Unlocking.."
+					end
+				end
+			end
+
+			local displayText = j.status
+		
+		
+			if j.isAuth then
+				displayText = "~g~E~w~ - " .. displayText
+			end
+				DrawText3Ds(j.x, j.y, j.z, displayText)
+
+		end
+		
+	end
+end)
+
+
+Citizen.CreateThread(function()
+	while true do
+		Citizen.Wait(1000)
+		local playerCoords = GetEntityCoords(GetPlayerPed(-1))
+		local distance, objCoords
+		local withinreach
+		
+		drowtargetdoors = {}
+			for k,doorID in ipairs(QB.Doors) do
+
+
+					if doorID.size then
+						size = doorID.size
+					end
+
+				if doorID.doors then
+					distance = #(playerCoords - doorID.doors[1].objCoords)
+					objCoords = doorID.textCoords
+				else
+					distance = #(playerCoords - doorID.objCoords)
+					objCoords = doorID.objCoords
+				end
+
+				if doorID.distance then
+					withinreach = doorID.distance
+					else
+						withinreach = maxDistance
+				end
+
+				if distance < 50 then
+
+					if doorID.doors then
+						for _,v in ipairs(doorID.doors) do
+							FreezeEntityPosition(v.object, doorID.locked)
+	
+							if doorID.locked and v.objYaw and GetEntityRotation(v.object).z ~= v.objYaw then
+								SetEntityRotation(v.object, 0.0, 0.0, v.objYaw, 2, true)
+							end
+						end
+					else
+						FreezeEntityPosition(doorID.object, doorID.locked)
+	
+						if doorID.locked and doorID.objYaw and GetEntityRotation(doorID.object).z ~= doorID.objYaw then
+							SetEntityRotation(doorID.object, 0.0, 0.0, doorID.objYaw, 2, true)
+						end
+					end
+				end
+
+
+				if distance < withinreach then
+					local aux = {}
+					local isAuthorized = IsAuthorized(doorID)
+					aux['id'] = k
+					aux['x'] = objCoords.x
+					aux['y'] = objCoords.y 
+					aux['z'] = objCoords.z 
+					aux['isAuth'] = IsAuthorized(doorID)
+
+					if doorID.locked then
+						aux['status'] = "Locked" 
+						if doorID.locking then
+							aux['status'] = "Unlocking.."
+						end
+					elseif not doorID.locked then
+						aux['status'] = "Unlocked"
+						if doorID.locking then
+							aux['status'] = "Locking.."
+						end
+					end
+
+					aux['doorID'] = doorID 
+					-- print("Add door with id:", k)
+					table.insert(drowtargetdoors, aux)
+					-- drowtargetdoors.k = aux
+
+
+
+				end
+
+			end
+	end
+end)
+--[[
+Citizen.CreateThread(function()
+	while true do
+		Citizen.Wait(0)
+		local playerCoords, awayFromDoors = GetEntityCoords(GetPlayerPed(-1)), true
 
 		for k,doorID in ipairs(QB.Doors) do
 			local distance
@@ -63,7 +178,7 @@ Citizen.CreateThread(function()
 			if doorID.distance then
 				maxDistance = doorID.distance
 			end
-			if distance < 5 then
+			if distance < 50 then
 				awayFromDoors = false
 				if doorID.doors then
 					for _,v in ipairs(doorID.doors) do
@@ -133,6 +248,31 @@ Citizen.CreateThread(function()
 		end
 	end
 end)
+--]]
+
+
+-- local props = {
+-- 	"prison_prop_door1",
+-- 	"prison_prop_door2",
+-- 	"v_ilev_gtdoor",
+-- 	"prison_prop_door1a"
+-- }
+
+-- Citizen.CreateThread(function()
+-- 	while true do
+-- 		for k, v in pairs(props) do
+-- 			local ped = GetPlayerPed(-1)
+-- 			local pos = GetEntityCoords(ped)
+-- 			local ClosestDoor = GetClosestObjectOfType(pos.x, pos.y, pos.z, 5.0, GetHashKey(v), 0, 0, 0)
+-- 			if ClosestDoor ~= 0 then
+-- 				local DoorCoords = GetEntityCoords(ClosestDoor)
+	
+-- 				DrawText3Ds(DoorCoords.x, DoorCoords.y, DoorCoords.z, "OBJ: "..v..", x: "..round(DoorCoords.x, 0)..", y: "..round(DoorCoords.y, 0)..", z: "..round(DoorCoords.z, 0))
+-- 			end
+-- 		end
+-- 		Citizen.Wait(1)
+-- 	end
+-- end)
 
 function round(num, numDecimalPlaces)
 	local mult = 10^(numDecimalPlaces or 0)
@@ -141,10 +281,11 @@ end
 
 RegisterNetEvent('lockpicks:UseLockpick')
 AddEventHandler('lockpicks:UseLockpick', function()
-	local pos = GetEntityCoords(PlayerPedId())
+	local ped = GetPlayerPed(-1)
+	local pos = GetEntityCoords(ped)
 	QBCore.Functions.TriggerCallback('qb-radio:server:GetItem', function(hasItem)
 		for k, v in pairs(QB.Doors) do
-			local dist = #(pos - vector3(QB.Doors[k].textCoords.x, QB.Doors[k].textCoords.y, QB.Doors[k].textCoords.z))
+			local dist = GetDistanceBetweenCoords(pos, QB.Doors[k].textCoords.x, QB.Doors[k].textCoords.y, QB.Doors[k].textCoords.z)
 			if dist < 1.5 then
 				if QB.Doors[k].pickable then
 					if QB.Doors[k].locked then
@@ -152,13 +293,13 @@ AddEventHandler('lockpicks:UseLockpick', function()
 							closestDoorKey, closestDoorValue = k, v
 							TriggerEvent('qb-lockpick:client:openLockpick', lockpickFinish)
 						else
-							QBCore.Functions.Notify("You Dont Have A Screwdriver Set", "error")
+							QBCore.Functions.Notify("You are missing a toolkit..", "error")
 						end
 					else
-						QBCore.Functions.Notify('The Door Is Not Locked', 'error', 2500)
+						QBCore.Functions.Notify('The door is already unlocked??', 'error', 2500)
 					end
 				else
-					QBCore.Functions.Notify('This Door Can Not Be Lockpicked', 'error', 2500)
+					QBCore.Functions.Notify('The door lock is too strong', 'error', 2500)
 				end
 			end
 		end
@@ -167,10 +308,10 @@ end)
 
 function lockpickFinish(success)
     if success then
-		QBCore.Functions.Notify('Success!', 'success', 2500)
+		QBCore.Functions.Notify('Succes!', 'success', 2500)
 		setDoorLocking(closestDoorValue, closestDoorKey)
     else
-        QBCore.Functions.Notify('Failed', 'error', 2500)
+        QBCore.Functions.Notify('Failed..', 'error', 2500)
     end
 end
 
@@ -199,15 +340,20 @@ function IsAuthorized(doorID)
 			return true
 		end
 	end
+	for _,gang in pairs(doorID.authorizedJobs) do
+		if gang == PlayerData.job.name then
+			return true
+		end
+	end
 	
 	return false
 end
 
 function openDoorAnim()
     loadAnimDict("anim@heists@keycard@") 
-    TaskPlayAnim( PlayerPedId(), "anim@heists@keycard@", "exit", 5.0, 1.0, -1, 16, 0, 0, 0, 0 )
+    TaskPlayAnim( GetPlayerPed(-1), "anim@heists@keycard@", "exit", 5.0, 1.0, -1, 16, 0, 0, 0, 0 )
 	SetTimeout(400, function()
-		ClearPedTasks(PlayerPedId())
+		ClearPedTasks(GetPlayerPed(-1))
 	end)
 end
 
